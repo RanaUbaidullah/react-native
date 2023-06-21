f9b90e1b216fbcfb12576ac98187de9a74d97bcb
Ubaidullah <researchfigma@gmail.com>
2023-06-21 15:56:22 +0500

    Update App.js

--- App/App.js
+++ App/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ubaidullah</Text>
+      <Text style={styles.container}>Ubaidullah</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 100,
+    color: 'red'
   },
 });
